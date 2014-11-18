74c1d9d5b7918db8ad7686f379d61ec206b73f8a
Rémy Abi-Khalil <remyabi@gmail.com>
2014-11-18 18:22:41 +0100

    Refs #METH-356 ~ Fixtures updated

---
+++ DataFixtures/ORM/FixturesRole.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace CanalTP\SamEcoreUserManagerBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+use CanalTP\SamCoreBundle\DataFixtures\ORM\RoleTrait;
+
+class FixturesRole extends AbstractFixture implements OrderedFixtureInterface
+{
+    use RoleTrait;
+
+    private $roles = array(
+        array(
+            'name'          => 'Utilisateur',
+            'reference'     => 'user-sam',
+            'application'   => 'app-samcore',
+            'isEditable'    => true,
+            'permissions'   => array(
+                'BUSINESS_VIEW_USER',
+                'BUSINESS_MANAGE_USER',
+                'BUSINESS_MANAGE_USER_ROLE',
+                'BUSINESS_MANAGE_USER_PERIMETER',
+                'BUSINESS_VIEW_ROLE',
+                'BUSINESS_MANAGE_ROLE'
+            )
+        ),
+        array(
+            'name'          => 'Référent',
+            'reference'     => 'referent-sam',
+            'application'   => 'app-samcore',
+            'isEditable'    => true,
+            'permissions'  => array()
+        ),
+        array(
+            'name'          => 'Observateur',
+            'reference'     => 'obsevateur-sam',
+            'application'   => 'app-samcore',
+            'isEditable'    => true,
+            'permissions'  => array()
+        ),
+        array(
+            'name'          => 'Administrateur',
+            'reference'     => 'admin-sam',
+            'application'   => 'app-samcore',
+            'isEditable'    => true,
+            'permissions'  => array(
+                'BUSINESS_VIEW_USER',
+                'BUSINESS_MANAGE_USER',
+                'BUSINESS_MANAGE_USER_ROLE',
+                'BUSINESS_MANAGE_USER_PERIMETER',
+                'BUSINESS_MANAGE_PERMISSION',
+                'BUSINESS_VIEW_ROLE',
+                'BUSINESS_MANAGE_ROLE'
+            )
+        ),
+        array(
+            'name'          => 'Super Admin',
+            'reference'     => 'super-admin-sam',
+            'application'   => 'app-samcore',
+            'isEditable'    => true,
+            'permissions'  => array(
+                'BUSINESS_VIEW_USER',
+                'BUSINESS_MANAGE_USER',
+                'BUSINESS_MANAGE_USER_ROLE',
+                'BUSINESS_MANAGE_USER_PERIMETER',
+                'BUSINESS_MANAGE_PERMISSION',
+                'BUSINESS_VIEW_ROLE',
+                'BUSINESS_MANAGE_ROLE',
+            )
+        )
+    );
+
+    public function load(ObjectManager $om)
+    {
+         foreach ($this->roles as $role) {
+            $this->createApplicationRole($om,  $role);
+        }
+        $om->flush();
+    }
+
+    /**
+    * {@inheritDoc}
+    */
+    public function getOrder()
+    {
+        return 2;
+    }
+}
+ 
--- DataFixtures/ORM/FixturesUser.php
+++ DataFixtures/ORM/FixturesUser.php
@@ -6,66 +6,41 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 
-use CanalTP\SamEcoreUserManagerBundle\Entity\User;
+use CanalTP\SamCoreBundle\DataFixtures\ORM\UserTrait;
 
 class FixturesUser extends AbstractFixture implements OrderedFixtureInterface
 {
-    private function createUser($data)
-    {
-        $user = new User();
-        $user->setUsername($data['username']);
-        $user->setFirstName($data['firstname']);
-        $user->setLastName($data['lastname']);
-        $user->setEnabled(true);
-        $user->setEmail($data['email']);
-        $user->setPlainPassword($data['password']);
+    use UserTrait;
 
-        if (isset($data['roles'])) {
-            foreach ($data['roles'] as $role) {
-                $user->addUserRole($this->getReference($role));
-            }
-        }
-
-        return $user;
-    }
-
-    public function load(ObjectManager $em)
-    {
-        //TODO:
-        $users = array(
-            array(
-                'username'  => 'Sam',
-                'firstname' => 'Samuel',
-                'lastname'  => 'Dictator',
-                'email'     => 'sam@canaltp.fr',
-                'password'  => 'sam',
-                'roles'     => array(
-                    'role-super-admin-sam',
-                    'role-user-sam',
-                    'role-referent-sam',
-                    'role-obs-sam',
-                    'role-admin-sam',
-                    'role-obs-mtt',
-                    'role-user-mtt',
-                    'role-admin-mtt',
-                    'ROLE_ADMIN_MATRIX',
-                    'ROLE_USER_MATRIX',
-                    'ROLE_ROOT_REAL_TIME',
-                    'ROLE_ADMIN_REAL_TIME',
-                    'ROLE_USER_REAL_TIME'
-                )
+    private $users = array(
+        array(
+            'id'        => null,
+            'username'  => 'Admin',
+            'firstname' => 'Super Admin',
+            'lastname'  => 'Super Admin',
+            'email'     => 'admin@canaltp.fr',
+            'password'  => 'admin',
+            'roles'     => array(
+                'role-super-admin-sam',
+                'role-admin-sam',
+                'role-user-sam',
+                'role-referent-sam',
+                'role-obsevateur-sam',
+                'role-admin-mtt',
+                'role-admin-realtime',
+                'role-admin-matrix',
+                'role-admin-nmp'
             ),
-        );
+            'customer'  => 'customer-canaltp'
+        )
+    );
 
-        foreach ($users as $user) {
-            $entity = $this->createUser($user);
-
-            $em->persist($entity);
-
-            $this->addReference('user-'.$user['username'], $entity);
+    public function load(ObjectManager $om)
+    {
+        foreach ($this->users as $userData) {
+            $userEntity = $this->createUser($om, $userData);
         }
-
-        $em->flush();
+        $om->flush();
     }
 
     /**
@@ -73,6 +48,6 @@ class FixturesUser extends AbstractFixture implements OrderedFixtureInterface
     */
     public function getOrder()
     {
-        return 3;
+        return 4;
     }
 }
