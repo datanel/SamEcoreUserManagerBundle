6e7fbc87c159a763c8850714f26fc58ec3c766cb
Nicolas Berard <nicolas.berard@canaltp.fr>
2016-09-22 12:00:27 +0200

    add functionnal tests for password validation rules

---
+++ Features/Context/FeatureContext.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace CanalTP\SamEcoreUserManagerBundle\Features\Context;
+
+use Behat\Behat\Hook\Scope\AfterScenarioScope;
+use Behat\MinkExtension\Context\MinkContext;
+
+class FeatureContext extends MinkContext
+{
+    /**
+     * @AfterScenario
+     * @param AfterScenarioScope $scope
+     */
+    public function afterScenario(AfterScenarioScope $scope)
+    {
+        $environment = $scope->getEnvironment();
+        $doctrineDbalContext = $environment->getContext('CanalTP\SamCoreBundle\Features\Context\DoctrineDbalContext');
+
+        $doctrineDbalContext->truncateTables([
+            'public.t_user_usr',
+        ]);
+    }
+
+    /**
+     * @Then /^I get an error saying "(?P<message>[^"]+)" on the field "(?P<field>[^"]+)"$/
+     */
+    public function iSeeError($message, $field)
+    {
+        $this->assertElementContainsText('.has-error label', $field);
+        $this->assertElementContainsText('.has-error ul.help-block', $message);
+    }
+
+}
